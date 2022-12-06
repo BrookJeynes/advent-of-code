5ca2acc28abd52af59eb9ecb34d40ef47e59e290
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2022-12-06 16:53:30 +1000

    using options for day 6

--- 2022/day-6/src/main.rs
+++ 2022/day-6/src/main.rs
@@ -3,36 +3,36 @@ use std::{fs, collections::HashSet};
 fn main() {
     let input: String = fs::read_to_string("input.txt").unwrap();
 
-    let answer_one = part_one(&input);
-    let answer_two = part_two(&input);
+    let answer_one = part_one(&input).unwrap();
+    let answer_two = part_two(&input).unwrap();
 
     println!("Answer one: {}\nAnswer two: {}", answer_one, answer_two);
 }
 
-fn part_one(input: &String) -> i32 {
+fn part_one(input: &String) -> Option<usize> {
     const SEQUENCE_LEN: usize = 4;
 
     for checked in 0..(input.len()-SEQUENCE_LEN) {
         let range = checked..(checked + SEQUENCE_LEN);
 
         if input[range].chars().collect::<HashSet<char>>().len() == SEQUENCE_LEN {
-            return (checked + SEQUENCE_LEN) as i32
+            return Some(checked + SEQUENCE_LEN)
         }
     }
     
-    -1
+    None
 }
 
-fn part_two(input: &String) -> i32 {
+fn part_two(input: &String) -> Option<usize> {
     const SEQUENCE_LEN: usize = 14;
 
     for checked in 0..(input.len()-SEQUENCE_LEN) {
         let range = checked..(checked + SEQUENCE_LEN);
 
         if input[range].chars().collect::<HashSet<char>>().len() == SEQUENCE_LEN {
-            return (checked + SEQUENCE_LEN) as i32
+            return Some(checked + SEQUENCE_LEN)
         }
     }
     
-    -1
+    None
 }
